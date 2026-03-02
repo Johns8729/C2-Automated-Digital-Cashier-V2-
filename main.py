e75e6a9f5c50db46e4d034134a353a6ba195671c
Valery <Valejo0310@gmail.com>
2026-03-02 15:59:51 -0500

    Feature attempts control first draft, using a while loop and conditionals

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+"""Feature: attemtps control draft
+Made by: Valery Avila Ortega"""
+
 saldo = 1000
+pin = 1243
+
+print ("Cuenta con tres intentos para ingresar el pin adecuado")
+contador = 1
+while contador <= 3:
+    pin = int (input("Ingrese su pin:"))
+    if pin == 1234:
+        print ("Puede continuar.")
+    else:
+        print ("Pin incorrecto")
+        if contador == 3:
+            print ("Programa suspendido.")
+            print ("¿Tiene problemas con su pin? Comuniquese con nuestros asesores para más información.")
+        contador = contador +1
+
