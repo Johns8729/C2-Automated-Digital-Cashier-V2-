e6898bf8f0afb66f1d60832daaf996b1f626f76e
Brxynxr <breynermanga07@gmail.com>
2026-03-02 10:55:42 -0500

    V3 of the CODE

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ saldo = 1000
 while True:
     try:
         cantidad = int(input("Ingrese la cantidad a depositar: "))
-
+#Acá va el condicional if para que busca arreglar el error cuando se ingresa un número negativo o racional
             saldo += cantidad
             print("Depósito exitoso.")
             print("Su nuevo saldo es:", saldo)
