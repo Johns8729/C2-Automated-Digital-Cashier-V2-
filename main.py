c6a8bcd48f5f9df6f782adec4cf6f629ebedd16a
Brxynxr <breynermanga07@gmail.com>
2026-03-02 10:54:06 -0500

    V2 of the CODE

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ while True:
     try:
         cantidad = int(input("Ingrese la cantidad a depositar: "))
 
-        if cantidad < 0:
-            print("Error: La cantidad debe ser un número entero positivo.")
-        else:
             saldo += cantidad
             print("Depósito exitoso.")
             print("Su nuevo saldo es:", saldo)
