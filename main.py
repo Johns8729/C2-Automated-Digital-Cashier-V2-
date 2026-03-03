e1e82256597085c76ddf3555553a399812865a8d
Silvio Barrios <56333049+silvio1615@users.noreply.github.com>
2026-03-03 07:44:19 -0500

    second commit aux funcion
    Added daily withdrawal limit and updated withdrawal logic.

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 saldo = 100
+limite_diario_retiro=2000
+retiro_diario=0
+monto=0
+try:
+    monto =float(monto)
+    if retiro_diario + monto > limite_diario_retiro:
+        disponible = limite_diario_retiro-retiro_diario
+        print(f"limite diario excedido. disponible hoy: ",disponible)
+        
+
+    saldo -= monto
+    retiro_diario += monto
+    print(f"✓ Retiro exitoso: ${monto:.2f}")
+    print(f"Saldo actual: ${saldo:.2f}")
+    
+
+except ValueError:
+    print("✗ Monto inválido")
