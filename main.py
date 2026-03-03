13a3564e9d49b7543d07686c43e76b45efdff20b
Yarde1101 <yardelis1101@gmail.com>
2026-03-03 06:56:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-saldo = 100
+# CONFIGURACION INICIAL, VAMOS A DETERMINAR LAS VARIABLES 
+print("BIENVENIDO AL CAJERO ATM TECH BANK RIWI DIGITAL")
+saldo =  1000
+pint_correcto = 1234
+intentos_maximos = 3 
+sesion_activa = False
+intentos = 0 
+cuenta_1 = 1234
+
+cuenta_1 = input("Ingrese su Nombr de Usuario: ")
+
+# Lo siguiente le tocaria a la persona de autenticacion
