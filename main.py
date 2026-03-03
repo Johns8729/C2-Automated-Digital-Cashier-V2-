5b0060082fb498db05ee790e13651ca0df89f07f
Oczo <corzooscar143@gmail.com>
2026-03-02 20:33:20 -0500

    V1 of the CODE
    Implement authentication process with PIN verification.

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-saldo = 100
+"""Feature: Athentication Process
+Made by: Oscar Corzo
+Date: 2024-06-01"""
+
+saldo = 1000
+pin = 1234
+
+while True:
+    try:
+        user_pin = int(input("Por favor, ingrese su PIN de autenticación para continuar: "))
+        if user_pin == pin:
+            print("Autenticación exitosa, puede continuar al menú principal.")
+            break
+        else:
+            print("PIN incorrecto. Inténtelo de nuevo.")
+
+#Espacio para la Feature: attemtps control draft
+
+    except ValueError:
+        print("Error: El PIN debe ser un número entero!")
+        
