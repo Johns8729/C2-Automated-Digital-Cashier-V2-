1c7ff37d9d62be7b29cfa96f1f26019340ca1b15
Leonardo Perez <leonardojeagerpt2005@gmail.com>
2026-03-04 07:23:59 -0500

    V1 OF THE CODE

--- main.py
+++ main.py
@@ -1,36 +1,19 @@
-# CONFIGURACION INICIAL, VAMOS A DETERMINAR LAS VARIABLES 
-print("BIENVENIDO AL CAJERO ATM TECH BANK RIWI DIGITAL")
-saldo =  1000
-pint_correcto = 1234
-intentos_maximos = 3 
-sesion_activa = False
-intentos = 0 
-cuenta_1 = 1234
+historial = ""
 
-cuenta_1 = input("Ingrese su Nombr de Usuario: ")
+print("Su saldo es:", saldo)
+historial = historial + "Consultó saldo\n"
 
-# Lo siguiente le tocaria a la persona de autenticacion
+saldo = saldo - retiro
+historial = historial + "Retiro de $" + str(retiro) + "\n"
 
-"""Feature: Athentication Process
-Made by: Oscar Corzo
-Date: 2024-06-01"""
+saldo = saldo + deposito
+historial = historial + "Depósito de $" + str(deposito) + "\n"
 
-while True:
-    try:
-        user_pin = int(input("~ Por favor, ingrese su PIN de autenticación para continuar:\n➤ "))
-        if user_pin == pin:
-            print("="*60)
-            print("Autenticación exitosa, puede continuar al menú principal.".center(60))
-            print("="*60)
-            break
-        else:
-            print("="*60)
-            print(f"PIN incorrecto. Inténtelo de nuevo.".center(60))
-            print("="*60)
-            
-# Espacio para la Feature: attemtps control
+print("\nHistorial de operaciones:")
 
-    except ValueError:
-        print("="*60)
-        print(f"Error: El PIN debe ser un número entero!".center(60))
-        print("="*60)
+if historial == "":
+    print("No se realizaron operaciones")
+else:
+    print("----------------------------------")
+    print(f"=========={historial}==========")
+    print("----------------------------------")
