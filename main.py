f09a1a422be54a7d556aa63576598f5a65df2d82
Yarde1101 <yardelis1101@gmail.com>
2026-03-05 07:41:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 # CONFIGURACION INICIAL, VAMOS A DETERMINAR LAS VARIABLES 
-print("BIENVENIDO AL CAJERO ATM TECH BANK RIWI DIGITAL")
+from time import sleep
+welcome = """
+╔════════════════════════════════════════════════════════════╗
+    🏦 BIENVENIDO AL CAJERO ATM TECH BANK RIWI DIGITAL 🏦 
+╚════════════════════════════════════════════════════════════╝
+"""
+for _ in welcome:
+    print(_,end="",flush=True)
+    sleep(0.005)
+
 saldo =  1000
 pint_correcto = 1234
 intentos_maximos = 3 
 sesion_activa = False
 intentos = 0 
 cuenta_1 = 1234
-
-cuenta_1 = input("Ingrese su Nombr de Usuario: ")
-
-# Lo siguiente le tocaria a la persona de autenticacion
+limite_diario_retiro=800 
+retiro_diario=0
+monto = 0
+historial = ""
+cuenta_1 = input("🤵 Ingrese su Nombre de Usuario: ")
