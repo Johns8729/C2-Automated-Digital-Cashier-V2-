f2f8c97f4b679b6fa24f91ad8ad707dcaf6b2e07
Habith <ing.hdeleon1@gmail.com>
2026-03-05 15:47:37 -0500

    Ingreso a la segunda opcion

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-elif Opcion == 2:
-        try:
-            monto_retirar = int(input("\ningrese el monto a retirar: "))
-            
-        #aqui van los condicionales
-
-            saldo -= monto_retirar
-            print(F"\nRetiro exitoso. Su nuevo saldo actualizado es de: {saldo}\n")
-            print("\nGracias por usar el cajero automático")
+    if Opcion == 2:
+        while True:    
+            try:
+                monto_retirar = int(input("\ningrese el monto a retirar: "))
+             
