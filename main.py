5524248aac7ad79506995e61ed11dd190f1d89f5
Brxynxr <breynermanga07@gmail.com>
2026-03-05 08:31:39 -0500

    final version of the code

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-saldo = 1000
-
-while True:
-    try:
-        cantidad = int(input("Ingrese la cantidad a depositar: "))
-#Acá va el condicional if para que busca arreglar el error cuando se ingresa un número negativo o racional
-            saldo += cantidad
-            print("Depósito exitoso.")
-            print("Su nuevo saldo es:", saldo)
-            break
-
-    except ValueError:
-        print("Error: Debe ingresar un número entero válido.")
+#breyner funcion depositar
+ elif Opcion == 3:
+        while True:
+            try:
+                cantidad = int(input("Ingrese la cantidad a depositar: "))
+                def VALIDACION_DEPOSITO(monto):
+                    if monto <= 0:
+                        return False, "No se permiten montos negativos o cero"
+                    return True, "Depòsito Valido!."
+                saldo = saldo + cantidad
+                historial = historial + "💰 Depósito de $" + str(cantidad) + "\n"
+                print(f"\nSu nuevo saldo es de: ${saldo}")
+                print("\nGracias por usar el cajero automático")
+                break
+            except ValueError:
+                print("Error: Debe ingresar un número entero válido.")
