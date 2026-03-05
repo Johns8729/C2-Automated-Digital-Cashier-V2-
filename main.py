82d3d502761a77c7c49a612bd62e116f722b40fd
jafet <tjafet93@gmail.com>
2026-03-05 09:10:38 -0500

    FINAL VERSION OF THE CODE

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
-saldo = 1000
+#(Jafet) FUNCION MENU
+welcome = """
+╔════════════════════════════════════════════════════════════╗
+                🏦 TECH BANK RIWI DIGITAL 🏦
+            🙌¡Bienvenido al Cajero Automático!🙌
+╚════════════════════════════════════════════════════════════╝
+    """
+for _ in welcome:
+    print(_,end="",flush=True)
+    sleep(0.005)
 
-print("="*50)
-print(f"{'🏦 TECH BANK RIWI DIGITAL':^50}")
-print("-"*50)
-print(f"{'🙌¡Bienvenido al Cajero Automático!🙌':^50}")
-print("-"*50)
-Cant_Operaciones = int(input(f"{'¿Cuántas operaciones desea realizar?':^50}"))
+Cant_Operaciones = int(input(f"{'¿Cuántas operaciones desea realizar?':^55}"))
+while Cant_Operaciones <= 0:
+    print (f"\n{'❌ Error: Ingrese un valor positivo ❌':^65}\n")
+    Cant_Operaciones = int(input(f"{'¿Cuántas operaciones desea realizar?':^65}"))
+
 for i in range (Cant_Operaciones) :
     Conteo = i + 1
-    Opcion = int(input(f"\n --------------- Menú Principal --------------- | #{Conteo}  \n 1) 🔍 Consultar saldo \n 2) 💳 Retirar dinero \n 3) 💰 Depositar dinero \n 4) ✅ Finalizar\n==================================================\n Seleccione una opción \n ➤ "))
+    Opcion = int(input(f"""--------------- Menú Principal --------------- | #{Conteo} 
+1) 🔍 Consultar saldo 
+2) 💳 Retirar dinero 
+3) 💰 Depositar dinero 
+4) ✅ Finalizar
+==================================================
+Seleccione una opción:
+➤ """))       
     while Opcion < 1 or Opcion > 4:
         print("\nERROR: Por favor ingrese una opción válida")
-        Opcion = int(input(f"\n --------------- Menú Principal --------------- | #{Conteo}  \n 1) 🔍 Consultar saldo \n 2) 💳 Retirar dinero \n 3) 💰 Depositar dinero \n 4) ✅ Finalizar \n==================================================\n Seleccione una opción \n ➤ "))
+        Opcion = int(input(f"""--------------- Menú Principal --------------- | #{Conteo} 
+1) 🔍 Consultar saldo 
+2) 💳 Retirar dinero 
+3) 💰 Depositar dinero 
+4) ✅ Finalizar
+==================================================
+Seleccione una opción:
+➤ """))
