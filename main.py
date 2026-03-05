5dc1334d5450a2bcf2e333cfd06920f96101c9fa
Oczo <corzooscar143@gmail.com>
2026-03-05 08:35:19 -0500

    Final Version of th CODE
    Refactor authentication process to limit PIN attempts.

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
 """Feature: Athentication Process
 Made by: Oscar Corzo
-Date: 2024-06-01"""
+Date: 2026-03-05"""
 
-saldo = 1000
-pin = 1234
+pin_correcto = 1234
+intentos = 0
+max_intentos = 3
 
-while True:
+while intentos < max_intentos:
     try:
         user_pin = int(input("~ Por favor, ingrese su PIN de autenticación para continuar:\n➤ "))
-        if user_pin == pin:
-            print("="*60)
-            print("Autenticación exitosa, puede continuar al menú principal.".center(60))
-            print("="*60)
-            break
-        else:
-            print("="*60)
-            print(f"PIN incorrecto. Inténtelo de nuevo.".center(60))
-            print("="*60)
-            
-# Espacio para la Feature: attemtps control
-
-    except ValueError:
-        print("="*60)
-        print(f"Error: El PIN debe ser un número entero!".center(60))
-        print("="*60)
