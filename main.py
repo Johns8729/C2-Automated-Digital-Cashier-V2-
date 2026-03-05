775fbdab51005b2047ef039f3fb6db41df97f6d1
Leonardo Perez <leonardojeagerpt2005@gmail.com>
2026-03-05 09:06:59 -0500

    V4 OF THE CODE

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-historial = ""
-
-print("Su saldo es:", saldo)
-historial = historial + "Consultó saldo\n"
-
-saldo = saldo - retiro
-historial = historial + "Retiro de $" + str(retiro) + "\n"
-
-saldo = saldo + deposito
-historial = historial + "Depósito de $" + str(deposito) + "\n"
-
-print("\nHistorial de operaciones:")
+(Leonardo)
+print("\n 🗂️  Historial de operaciones :")
 
 if historial == "":
-    print("No se realizaron operaciones")
+    print("❌ No se realizaron operaciones ❌")
 else:
-    print("--------------------------------")
-    print(f"=========={historial}==========")
-    print("--------------------------------")
+    print(f"""============================================================
+{historial}============================================================""")
