0a2e3fd7e196f9eaf348109e3fbdb7788d7148ab
Habith <ing.hdeleon1@gmail.com>
2026-03-05 15:50:10 -0500

    Contador del saldo a retirar + print

--- main.py
+++ main.py
@@ -2,4 +2,7 @@
         while True:    
             try:
                 monto_retirar = int(input("\ningrese el monto a retirar: "))
-             
+
+                saldo -= monto_retirar
+                print(F"\nRetiro exitoso. Su nuevo saldo actualizado es de: {saldo}\n")
+                
