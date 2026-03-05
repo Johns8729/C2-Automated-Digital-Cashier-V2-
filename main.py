625547d8c031fe9fabc24e9999dcc3b04046ed55
Silvio Barrios <56333049+silvio1615@users.noreply.github.com>
2026-03-05 09:03:06 -0500

    Version FInal
    corrección de orden de ejecucion de las instrucciones de los contadores como los print se añadio un extra de poder mostrar la hora tanto en el llamado de la opcion 1 como la 2

--- main.py
+++ main.py
@@ -1,15 +1,61 @@
-saldo = 100
-limite_diario_retiro=800 
+import datetime
+import time
+saldo = 2000
+limite_diario_retiro=1500
 retiro_diario=0
-elif Opcion == 2:
-    monto_retirar = int(input("\ningrese el monto a retirar: "))
 
-    if retiro_diario+monto_retirar > limite_diario_retiro:
-     print("Usted ha excedido el límite diario de retiro.")
-    
-    else:
-        print("limite diario disponible hoy: ", limite_diario_retiro)
-        saldo -= monto_retirar
-        retiro_diario += monto_retirar
-        print(F"\nRetiro exitoso. Su nuevo saldo actualizado es de: {saldo}\n")
-        print("\nGracias por usar el cajero automático")
+print("="*50)
+print(f"{'🏦 TECH BANK RIWI DIGITAL':^50}")
+print("-"*50)
+print(f"{'🙌¡Bienvenido al Cajero Automático!🙌':^50}")
+print("-"*50)
+Cant_Operaciones = int(input(f"{'¿Cuántas operaciones desea realizar?':^50}"))
+for i in range (Cant_Operaciones) :
+    Conteo = i + 1
+    Opcion = int(input(f"\n --------------- Menú Principal --------------- | #{Conteo}  \n 1) 🔍 Consultar saldo \n 2) 💳 Retirar dinero \n 3) 💰 Depositar dinero \n 4) ✅ Finalizar\n==================================================\n Seleccione una opción \n ➤ "))
+    while Opcion < 1 or Opcion > 4:
+        print("\nERROR: Por favor ingrese una opción válida")
+        Opcion = int(input(f"\n --------------- Menú Principal --------------- | #{Conteo}  \n 1) 🔍 Consultar saldo \n 2) 💳 Retirar dinero \n 3) 💰 Depositar dinero \n 4) ✅ Finalizar \n==================================================\n Seleccione una opción \n ➤ "))
+    if Opcion == 1:
+        fecha_actual = datetime.date.today()
+        hora_actual = time.strftime("%H:%M:%S")
+        print("fecha: ",fecha_actual)
+        print("hora: ",hora_actual)
+        print("Tu saldo actual es: ", saldo )
+        print("retiro relizados hoy: ", retiro_diario)
+        print("retiro diario disponible hoy: ", limite_diario_retiro-retiro_diario)
+        print("-"*30)
+        print("\n")
+
+
+    if Opcion == 2:
+            try:
+                monto_retirar = int(input("\ningrese el monto a retirar: "))
+             
+                while monto_retirar <= 0:
+                    monto_retirar = int(input("El monto a retirar debe ser mayor que cero.Ingrese un nuevo monto: "))
+
+                if monto_retirar > saldo:   
+                   print("✗ Fondos insuficientes.saldo actual: ", saldo)
+                   continue
+                if retiro_diario+monto_retirar > limite_diario_retiro:
+                   disponible = limite_diario_retiro-retiro_diario
+                   print(f"✗ Límite diario de retiro excedido. Disponible hoy: ${disponible:.2f}")
+                   continue  
+                saldo -= monto_retirar
+                retiro_diario += monto_retirar
+                fecha_actual = datetime.date.today()
+                hora_actual = time.strftime("%H:%M:%S")
+                print("fecha: ",fecha_actual)
+                print("hora: ",hora_actual)
+                print(F"\nRetiro exitoso. Su nuevo saldo actualizado es de: {saldo}\n")
+                print("limite  disponible hoy: ", limite_diario_retiro-retiro_diario)
+                print("\nGracias por usar el cajero automático")
+                
+
+            except ValueError:
+                print("✗ Monto inválido")
+                
+                    
+                
+            
