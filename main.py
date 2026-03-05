f6d05e60bff3ccec81878d1202a1335bba5611fc
Juanesco100 <juanse01sebas@gmail.com>
2026-03-05 09:41:58 -0500

    Final version

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 
 if Opcion == 1:
 
-        print("Tu saldo actual es: ", saldo )
-        print("-"*30)
-        print("\n")
+            print("Tu saldo actual es: ", saldo )
+            print("-"*30)
+            print("\n")
+            historial = historial + "🔍 Consultó saldo\n"
         
         
         
